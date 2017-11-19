2c810ad01dc39edf0b415f5b8ee139c1eb8612b3
gaisei makimura <futuredictator30@yahoo.co.jp>
2017-11-20 01:12:17 +0900

    グローバルナビ、サイドバーナビなど

--- wp-content/themes/apt-pc/header.php
+++ wp-content/themes/apt-pc/header.php
@@ -9,7 +9,7 @@
 <?php wp_head(); ?>
 </head>
 
-<body class="home">
+<body <?php body_class(); ?>>
 <div id="wrap">
   <div id="header" role="banner">
     <div class="inner clearfix">
@@ -17,9 +17,14 @@
         <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/site_id.png" width="322" height="42" alt="<?php bloginfo('description'); ?><?php bloginfo( 'name' ); ?>" /></a>
       </h1>
       <div class="utility">
-        <ul>
-          <li><a href="#">採用情報</a></li>
-        </ul>
+        
+        <?php
+        	wp_nav_menu(array(
+        		'container' => false,
+        		'theme_location' => 'place_pc_utility'
+        	));
+        ?>
+
         <div id="search" role="search">
           <form role="search" method="get" id="searchform" class="searchform" action="#">
             <div>
@@ -30,9 +35,34 @@
         </div><!-- #search end -->
       </div><!-- .utility end -->
     </div><!-- .inner end -->
-    <div id="global_nav">
-      <ul>
-        <li class="current-menu-item menu-item-slug-top"><a href="#">トップページ</a></li>
-      </ul>
-    </div>
+    
+	<?php
+
+		add_filter('nav_menu_css_class', 'apt_slug_nav', 10, 2);
+
+		wp_nav_menu(array(
+			'container' => 'div',
+			'container_id' => 'global_nav',
+			'theme_location' => 'place_pc_global',
+			'depth' => 3
+		));
+
+		remove_filter('nav_menu_css_class', 'apt_slug_nav');
+	?>
+
   </div><!-- #header end -->
+
+  <?php
+  	if(!is_front_page()):
+  ?>
+  <div class="category_image"><img src="<?php echo get_template_directory_uri(); ?>/images/sub/img_cat_def.png" width="950" height="120" alt=""></div><!-- /.category_image end -->
+
+  <?php
+  	if(class_exists('Wp_SiteManager_bread_crumb')):
+  		Wp_SiteManager_bread_crumb::bread_crumb('navi_element=div&elm_id=bread_crumb');
+  	endif;
+  ?>
+
+  <?php
+  	endif;
+  ?>
---
+++ wp-content/themes/apt-pc/page_top_link.php
@@ -0,0 +1,1 @@
+        <div class="clearfix" id="page_top_link"><a href="#">このページの先頭へ</a></div>
